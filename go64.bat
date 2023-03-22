4917a75bbfab95f59e4933630dc3c7758d6d935f
Antonio Linares <alinares@fivetechsoft.com>
2023-03-22 09:35:11 +0100

    minor changes

--- go64.bat
+++ go64.bat
@@ -1,4 +1,7 @@
+@set > env_vars.txt
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-set PATH=%PATH%;C:\Users\Anto\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts
+rem set PYTHONHOME=c:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\VC\SecurityIssueAnalysis\python
+rem set PYTHONPATH=c:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\VC\SecurityIssueAnalysis\python
 c:\harbour\bin\win\msvc64\hbmk2 test64.hbp -comp=msvc64
 test.exe
+@for /F %A in (aenv_vars.txt) do SET %A
