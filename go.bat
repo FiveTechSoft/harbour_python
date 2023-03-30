1cf884b5592a0cae47a492e7fb2a03dec926a925
Antonio Linares <alinares@fivetechsoft.com>
2023-03-30 09:13:18 +0200

    updated

--- go.bat
+++ go.bat
@@ -2,6 +2,6 @@ setlocal
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 set PYTHONHOME=c:\Users\Anto\AppData\Local\Programs\Python\Python311
 set PYTHONPATH=c:\Users\Anto\AppData\Local\Programs\Python\Python311
-c:\harbour\bin\win\msvc64\hbmk2 test64.hbp -comp=msvc64
-gui.exe
+c:\harbour\bin\win\msvc64\hbmk2 %1.prg test64.hbp -comp=msvc64
+%1.exe
 endlocal
